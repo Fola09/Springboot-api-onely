1d78751a63a8546d5e43f11be976f1b32a842b90
Ola Fola <folaolayiwola.fo@gmail.com>
2019-05-14 10:44:25 +0100

    First commit

---
+++ src/main/java/com/fola/springboot/apionely/springbootapionely/SpringbootApiOnelyApplication.java
@@ -0,0 +1,13 @@
+package com.fola.springboot.apionely.springbootapionely;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApiOnelyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApiOnelyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fola/springboot/apionely/springbootapionely/SpringbootApiOnelyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fola.springboot.apionely.springbootapionely;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootApiOnelyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
